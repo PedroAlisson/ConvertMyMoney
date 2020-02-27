452aeb90f0c72d05e986e4aed209ddaa156d9df4
PedroAlisson <pedrodevelope@gmail.com>
2020-02-27 19:02:07 -0300

    insert body-parse

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const app = express()
 const path = require('path')
 const convert = require('./lib/convert')
 const apiBCD = require('./lib/api.bcd')
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.get ('/', async(req,res) =>{
     const cotacao = await apiBCD.getCotacao()
@@ -15,9 +17,8 @@ app.get ('/', async(req,res) =>{
     })
 })
 
-app.get('/cotacao', (req,res) =>{
-    const {cotacao, quantidade} = req.query
-    
+app.post('/cotacao', (req,res) =>{
+    const {cotacao, quantidade} = req.body    
     if(cotacao && quantidade) {
     const conversao = convert.convert(cotacao, quantidade)
     res.render('cotacao',{
